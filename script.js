27a1bc345adadd3f48a1db65e1514fe6b53e200e
DiddySmooth <avery.mcclead@gmail.com>
2021-03-11 20:44:33 -0600

    did it

--- script.js
+++ script.js
@@ -6,6 +6,10 @@
     document.querySelector('.MelvinBox').classList.add('hidden')
     document.querySelector('.MiriamBox').classList.add('hidden')
     document.querySelector('.PatsyBox').classList.add('hidden')
+    document.querySelector('.JerryUserBox').classList.add("highlight")
+    document.querySelector('.MelvinUserBox').classList.remove('highlight')
+    document.querySelector('.MiriamUserBox').classList.remove('highlight')
+    document.querySelector('.PatsyUserBox').classList.remove('highlight')
   })
   
   document.querySelector('#Melvin').addEventListener('click', () => {
@@ -13,6 +17,10 @@
     document.querySelector('.MelvinBox').classList.remove('hidden')
     document.querySelector('.MiriamBox').classList.add('hidden')
     document.querySelector('.PatsyBox').classList.add('hidden')
+    document.querySelector('.JerryUserBox').classList.remove("highlight")
+    document.querySelector('.MelvinUserBox').classList.add('highlight')
+    document.querySelector('.MiriamUserBox').classList.remove('highlight')
+    document.querySelector('.PatsyUserBox').classList.remove('highlight')
   })
   
   document.querySelector('#Miriam').addEventListener('click', () => {
@@ -20,10 +28,19 @@
     document.querySelector('.MelvinBox').classList.add('hidden')
     document.querySelector('.MiriamBox').classList.remove('hidden')
     document.querySelector('.PatsyBox').classList.add('hidden')
+    document.querySelector('.JerryUserBox').classList.remove("highlight")
+    document.querySelector('.MelvinUserBox').classList.remove('highlight')
+    document.querySelector('.MiriamUserBox').classList.add('highlight')
+    document.querySelector('.PatsyUserBox').classList.remove('highlight')
   })
+
   document.querySelector('#Patsy').addEventListener('click', () => {
     document.querySelector('.JerryBox').classList.add('hidden')
     document.querySelector('.MelvinBox').classList.add('hidden')
     document.querySelector('.MiriamBox').classList.add('hidden')
     document.querySelector('.PatsyBox').classList.remove('hidden')
+    document.querySelector('.JerryUserBox').classList.remove("highlight")
+    document.querySelector('.MelvinUserBox').classList.remove('highlight')
+    document.querySelector('.MiriamUserBox').classList.remove('highlight')
+    document.querySelector('.PatsyUserBox').classList.add('highlight')
   })
